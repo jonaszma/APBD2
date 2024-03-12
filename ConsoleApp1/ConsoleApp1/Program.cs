12b8dfb10fbda997ee4606138a4d52528baef977
Jonasz Marczewski <s27202@pjwstk.edu.pl>
2024-03-12 13:29:49 +0100

    add avf method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,3 +2,15 @@
 
 Console.WriteLine("Hello, User!!!!");
 Console.WriteLine("Hello, User!!!!");
+int[] arr = new[] { 1, 2, 3, 4 };
+Console.WriteLine(GetAvg(arr));
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    foreach (var num in arr)
+    {
+        sum += num;
+    }
+
+    return (double)sum / arr.Length;
+}
