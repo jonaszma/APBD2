3c7fe16c3074b9dd7c6b6ebfc517c5d6646f825d
Jonasz <s27202@pjwstk.edu.pl>
2024-03-12 20:41:53 +0100

    dodano GetMax

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Hello, User!!!!");
 Console.WriteLine("Hello, User!!!!");
 int[] arr = new[] { 1, 2, 3, 4 };
 Console.WriteLine(GetAvg(arr));
+Console.WriteLine(GetMax(arr));
 static double GetAvg(int[] arr)
 {
     int sum = 0;
@@ -13,4 +14,18 @@ static double GetAvg(int[] arr)
     }
 
     return (double)sum / arr.Length;
+}
+
+static double GetMax(int[] arr)
+{
+    int sum = arr[0];
+    foreach (var num in arr)
+    {
+        if (num > sum)
+        {
+            sum = num;
+        }
+    }
+
+    return (double)sum;
 }
